d6d42f04dd7eb69c770361bbaee919611a2eb89b
Phuoc T. Nguyen <55012667+jesuispius@users.noreply.github.com>
2022-04-15 16:32:32 -0700

    done chat screen UI

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,7 +12,7 @@ export const Navbar = () => {
     return (
         <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
-                <a href="/" className="flex items-center text-black" style={{"textDecoration" : "none"}}>
+                <a href="/homepage" className="flex items-center text-black" style={{"textDecoration" : "none"}}>
                     <img src="security-camera.png" className="mr-3 h-6 sm:h-9" alt="Open Camera Logo" />
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">OpenCamera</span>
                 </a>
@@ -28,13 +28,13 @@ export const Navbar = () => {
                         </div>
                         <ul className="py-1" aria-labelledby="dropdown">
                             <li>
-                                <a href="/" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" style={{"textDecoration" : "none"}}>Profile</a>
+                                <a href="/homepage" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" style={{"textDecoration" : "none"}}>Profile</a>
                             </li>
                             <li>
-                                <a href="/" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" style={{"textDecoration" : "none"}}>Settings</a>
+                                <a href="/homepage" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" style={{"textDecoration" : "none"}}>Settings</a>
                             </li>
                             <li>
-                                <a href="/" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" style={{"textDecoration" : "none"}}>Logout</a>
+                                <a href="/homepage" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" style={{"textDecoration" : "none"}}>Logout</a>
                             </li>
                         </ul>
                     </div>
@@ -48,13 +48,13 @@ export const Navbar = () => {
                 <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
                     <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                         <li>
-                            <a href="/" className="block py-2 pr-4 pl-3 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 text-black" aria-current="page" style={{"textDecoration" : "none"}}>Home</a>
+                            <a href="/homepage" className="block py-2 pr-4 pl-3 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 text-black" aria-current="page" style={{"textDecoration" : "none"}}>Home</a>
                         </li>
                         <li>
-                            <a href="/" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" style={{"textDecoration" : "none"}}>Profile</a>
+                            <a href="/homepage" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" style={{"textDecoration" : "none"}}>Profile</a>
                         </li>
                         <li>
-                            <a href="/" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" style={{"textDecoration" : "none"}}>Settings</a>
+                            <a href="/homepage" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" style={{"textDecoration" : "none"}}>Settings</a>
                         </li>
                     </ul>
                 </div>
