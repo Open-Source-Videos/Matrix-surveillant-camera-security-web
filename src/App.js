5f8ec3dbb0cbf4b548b520573c33e2030eb32890
Phuoc T. Nguyen <55012667+jesuispius@users.noreply.github.com>
2022-04-15 12:09:20 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ const USERNAME = "@test003:pdxinfosec.org";
 const ROOM_ID = "!bdQMmkTBTMqUPAOvms:pdxinfosec.org";
 
 
-
 function App() {
     const [listImageURL, setListImageURL] = useState([]);
     const [listVideoURL, setListVideoURL] = useState([]);
@@ -209,128 +208,6 @@ function App() {
                     )}
 
 				<br/>
-				<div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
-					<div className="flex justify-center px-2">
-						<div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg lg:items-center">
-							<img className="w-full h-50 md:h-77 lg:h-auto object-cover md:w-72 rounded-t-lg md:rounded-none md:rounded-l-lg" src={"testimage.jpg"} alt="" />
-							<div className="p-6 flex flex-col justify-start">
-								<h5 className="text-gray-900 text-lg font-medium mb-2">Garage View</h5>
-								<p className="text-gray-700 text-base mb-4">Tue, Apr 12 2022 21:48:12</p>
-								<div className="grid grid-cols-2 gap-2 md:grid-cols-1">
-									<button type="button" className="bg-rose-500 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg border border-rose-500 inline-flex items-center justify-center">
-										<svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 22 22" stroke="currentColor" strokeWidth="2">
-											<path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-											<path strokeLinecap="round" strokeLinejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-										</svg>
-										<span>Watch</span>
-									</button>
-									<button className="bg-white hover:bg-rose-500 text-rose-500 hover:text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg border border-rose-500 inline-flex items-center justify-center">
-										<svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-											<path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
-										</svg>
-										<span>Download</span>
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="flex justify-center px-2">
-						<div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg lg:items-center">
-							<img className="w-full h-50 md:h-77 lg:h-auto object-cover md:w-72 rounded-t-lg md:rounded-none md:rounded-l-lg" src={"testimage.jpg"} alt="" />
-							<div className="p-6 flex flex-col justify-start">
-								<h5 className="text-gray-900 text-lg font-medium mb-2">Garage View</h5>
-								<p className="text-gray-700 text-base mb-4">Tue, Apr 12 2022 21:48:12</p>
-								<div className="grid grid-cols-2 gap-2 md:grid-cols-1">
-									<button type="button" className="bg-rose-500 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg border border-rose-500 inline-flex items-center justify-center">
-										<svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 22 22" stroke="currentColor" strokeWidth="2">
-											<path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-											<path strokeLinecap="round" strokeLinejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-										</svg>
-										<span>Watch</span>
-									</button>
-									<button className="bg-white hover:bg-rose-500 text-rose-500 hover:text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg border border-rose-500 inline-flex items-center justify-center">
-										<svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-											<path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
-										</svg>
-										<span>Download</span>
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="flex justify-center px-2">
-						<div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg lg:items-center">
-							<img className="w-full h-50 md:h-77 lg:h-auto object-cover md:w-72 rounded-t-lg md:rounded-none md:rounded-l-lg" src={"testimage.jpg"} alt="" />
-							<div className="p-6 flex flex-col justify-start">
-								<h5 className="text-gray-900 text-lg font-medium mb-2">Garage View</h5>
-								<p className="text-gray-700 text-base mb-4">Tue, Apr 12 2022 21:48:12</p>
-								<div className="grid grid-cols-2 gap-2 md:grid-cols-1">
-									<button type="button" className="bg-rose-500 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg border border-rose-500 inline-flex items-center justify-center">
-										<svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 22 22" stroke="currentColor" strokeWidth="2">
-											<path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-											<path strokeLinecap="round" strokeLinejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-										</svg>
-										<span>Watch</span>
-									</button>
-									<button className="bg-white hover:bg-rose-500 text-rose-500 hover:text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg border border-rose-500 inline-flex items-center justify-center">
-										<svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-											<path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
-										</svg>
-										<span>Download</span>
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="flex justify-center px-2">
-						<div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg lg:items-center">
-							<img className="w-full h-50 md:h-77 lg:h-auto object-cover md:w-72 rounded-t-lg md:rounded-none md:rounded-l-lg" src={"testimage.jpg"} alt="" />
-							<div className="p-6 flex flex-col justify-start">
-								<h5 className="text-gray-900 text-lg font-medium mb-2">Garage View</h5>
-								<p className="text-gray-700 text-base mb-4">Tue, Apr 12 2022 21:48:12</p>
-								<div className="grid grid-cols-2 gap-2 md:grid-cols-1">
-									<button type="button" className="bg-rose-500 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg border border-rose-500 inline-flex items-center justify-center">
-										<svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 22 22" stroke="currentColor" strokeWidth="2">
-											<path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-											<path strokeLinecap="round" strokeLinejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-										</svg>
-										<span>Watch</span>
-									</button>
-									<button className="bg-white hover:bg-rose-500 text-rose-500 hover:text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg border border-rose-500 inline-flex items-center justify-center">
-										<svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-											<path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
-										</svg>
-										<span>Download</span>
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="flex justify-center px-2">
-						<div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg lg:items-center">
-							<img className="w-full h-50 md:h-77 lg:h-auto object-cover md:w-72 rounded-t-lg md:rounded-none md:rounded-l-lg" src={"testimage.jpg"} alt="" />
-							<div className="p-6 flex flex-col justify-start">
-								<h5 className="text-gray-900 text-lg font-medium mb-2">Garage View</h5>
-								<p className="text-gray-700 text-base mb-4">Tue, Apr 12 2022 21:48:12</p>
-								<div className="grid grid-cols-2 gap-2 md:grid-cols-1">
-									<button type="button" className="bg-rose-500 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg border border-rose-500 inline-flex items-center justify-center">
-										<svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 22 22" stroke="currentColor" strokeWidth="2">
-											<path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-											<path strokeLinecap="round" strokeLinejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-										</svg>
-										<span>Watch</span>
-									</button>
-									<button className="bg-white hover:bg-rose-500 text-rose-500 hover:text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg border border-rose-500 inline-flex items-center justify-center">
-										<svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-											<path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
-										</svg>
-										<span>Download</span>
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
             </header>
         </div>
     );
