1fa55d0d1f87f80de3d53c570fe04dd2f6f57ca3
Tri Le <trilq1402@gmail.com>
2022-05-05 17:24:47 -0700

    fix 403 page

--- src/pages/Page403.js
+++ src/pages/Page403.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+var timeOut;
+const Page403 = () => {
+    const [delayRender, setDelayRender] = useState(true);
 
-class Page403 extends React.Component{
-    render(){
-        return (
-            <div className="container mx-auto h-screen">
-                <img 
-                    src={"403 Error Forbidden-bro.svg"} 
-                    alt="403 Error" 
-                    className="object-contain h-screen w-full" 
-                />
-                <div className="text-center">
-                    <Link to="/" className="text-red-600 text-decoration-none">Go to Home </Link>
+    useEffect(() => {
+        timeOut = setTimeout(() => {
+            setDelayRender(false);
+        }, 2000);
+
+        return () => {
+            clearTimeout(timeOut);
+        };
+    }, []);
+    return (
+        <>
+            {delayRender ? (
+                <></>
+            ) : (
+                <div className="container mx-auto h-screen">
+                    <img
+                        src={'403 Error Forbidden-bro.svg'}
+                        alt="403 Error"
+                        className="object-contain h-screen w-full"
+                    />
+                    <div className="text-center">
+                        <Link
+                            to="/"
+                            className="text-red-600 text-decoration-none"
+                        >
+                            Go to Home{' '}
+                        </Link>
+                    </div>
                 </div>
-            </div>
-        )
-    }
-}
+            )}
+        </>
+    );
+};
 export default Page403;
