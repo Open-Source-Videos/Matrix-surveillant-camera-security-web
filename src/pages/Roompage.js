6d173e04405fda5629341761e64ad2354464be80
Phuoc T. Nguyen <55012667+jesuispius@users.noreply.github.com>
2022-05-08 11:57:01 -0700

    Update Roompage.js

--- src/pages/Roompage.js
+++ src/pages/Roompage.js
@@ -1,7 +1,10 @@
 
-import { React, useState, useEffect } from 'react';
+import { 
+    React, 
+    useState, 
+    useEffect 
+} from 'react';
 import useMatrixClient from '../hooks/useMatrixClient';
-
 import Page403 from './Page403';
 import { useHistory } from "react-router-dom";
 
@@ -12,6 +15,11 @@ const RoomPage = ({roomList, avarta, displayName}) => {
     const [roomName, setRoomName] = useState(null);
     const history = useHistory();
     const { createRoom, inviteUserToRoom } = useMatrixClient();
+
+    
+    useEffect(() => {
+        console.log("ROOM LIST: ", roomList)
+    }, [])
 
     const handleCreateRoom = async (e) => {
         // e.preventDefault();
@@ -24,135 +32,147 @@ const RoomPage = ({roomList, avarta, displayName}) => {
         console.log('\n\n test devices:', deviceID);
     };
 
+
     const listItems = roomList.map((number) =>
-    <li class="border-gray-400 flex flex-row mb-4" onClick={() => history.push('/homepage')}>
-										<div
-											class="select-none flex flex-1 items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-2 rounded-2xl border-2 p-6 hover:shadow-2xl border-yellow-400"
-										>
-											<div class="flex-1 pl-1 mr-16">
-												<div class="font-medium">
-													{number.name}
-												</div>
-											</div>
-											<div
-												class="w-1/4 text-wrap text-center flex text-white text-bold flex-col rounded-md bg-yellow-400 justify-center items-center mr-10 p-2"
-											>
-												Join
-											</div>
-										</div>
-									</li>
-);
+        <li 
+            className="border-gray-400 flex flex-row mb-4" 
+            onClick={() => history.push('/homepage')}
+        >
+            <div
+                className="select-none flex flex-1 items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-2 rounded-2xl border-2 p-6 hover:shadow-2xl border-yellow-400"
+            >
+                <div className="flex-1 pl-1 mr-16">
+                    <div className="font-medium">
+                        {number.name}
+                    </div>
+                </div>
+                <div
+                    className="w-1/4 text-wrap text-center flex text-white text-bold flex-col rounded-md bg-yellow-400 justify-center items-center mr-10 p-2"
+                >
+                    Join
+                </div>
+            </div>
+        </li>
+    );
+
+
     return(
         <>
-                            <div className="px-6 mt-5">
-                                <div className="flex flex-wrap justify-center mt-5">
-                                        <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center mt-5">
-                                            <div className="relative">
-                                                <img
-                                                    alt="..."
-                                                    src={avarta}
-                                                    className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
-                                                    style={{ maxWidth: '150px' }}
-                                                />
-                                            </div>
-                                        </div>
+        <div className="px-6 mt-5">
+            <div className="flex flex-wrap justify-center mt-5">
+                <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center mt-5">
+                    <div className="relative">
+                        <img
+                            alt="..."
+                            src={avarta}
+                            className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
+                            style={{ maxWidth: '150px' }}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="text-center mt-12">
+            <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 pt-5">
+                {displayName}
+            </h3>
+        </div>
+        
+        
+
+        <div className="container mt-5 flex mx-auto w-full items-center justify-center">
+            <ul className="flex flex-col p-4">
+                {listItems}
+            </ul>
+        </div>
+
+        <div class="container flex mx-auto w-full items-center justify-center">
+            <button 
+                className="py-2 px-4 text-yellow-400 font-semibold border-2 p-6 border-yellow-400 rounded hover:bg-yellow-400 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0" 
+                type="button" 
+                onClick={() => setShowModal(true)}
+            >
+                    Create New Room
+            </button> 
+        </div>
+            {showModal ? (
+                <>
+                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="mt-4 flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                    <img 
+                                        className="mr-5 h-12 w-auto" 
+                                        src="icons8-tiger-96.png" 
+                                        alt="Workflow">
+                                    </img>
+                                    <h3 className="text-3xl font-semibold">
+                                        Add Room
+                                    </h3>
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                            ×
+                                        </span>
+                                    </button>
+                                </div>
+                                {/*body*/}
+                            
+                                <div className="relative p-6 flex-auto">
+                                    <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Room name</label>
+                                    <input 
+                                        type="text" 
+                                        id="text" 
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter room name" required
+                                        onChange={event => setRoomName(event.target.value)}
+                                    ></input>
+                                </div>
+                            
+
+                                <div className="relative p-6 flex-auto">
+                                    <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                                    I always felt like I could do anything. That’s the main
+                                    thing people are controlled by! Thoughts- their perception
+                                    of themselves! They're slowed down by their perception of
+                                    themselves. If you're taught you can’t do anything, you
+                                    won’t do anything. I was taught I could do everything.
+                                    </p>
+                                </div>
+        
+                            
+                                {/*footer*/}
+                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                    <button
+                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Close
+                                    </button>
+                                    <button
+                                        className="bg-yellow-400 text-white active:bg-yellow-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => {
+                                            handleCreateRoom(roomName); 
+                                            setShowModal(false)}
+                                        }
+                                    >
+                                        Save Changes
+                                    </button>
                                 </div>
                             </div>
-
-                            <div className="text-center mt-12">
-                                    <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 pt-5">
-                                        {displayName}
-                                    </h3>
-                            </div>
-                            
-                            
-
-                            <div class="container mt-5 flex mx-auto w-full items-center justify-center">
-                          
-								<ul class="flex flex-col p-4">
-                                    {listItems}
-								</ul>
-							</div>
-
-                            <div class="container flex mx-auto w-full items-center justify-center">
-               
-                                <button 
-                                className="py-2 px-4 text-yellow-400 font-semibold border-2 p-6 border-yellow-400 rounded hover:bg-yellow-400 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0" 
-                                type="button" 
-                                onClick={() => setShowModal(true)}
-                                >
-                                        Create New Room
-                                </button> 
-                            </div>
-                                {showModal ? (
-                                    <>
-                                           <div
-                                            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                                        >
-                                            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                                            {/*content*/}
-                                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                                {/*header*/}
-                                                <div className="mt-4 flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                                <img class="mr-5 h-12 w-auto" src="icons8-tiger-96.png" alt="Workflow"></img>
-                                                <h3 className="text-3xl font-semibold">
-                                                    Add Room
-                                                </h3>
-                                                <button
-                                                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                                    onClick={() => setShowModal(false)}
-                                                >
-                                                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                                    ×
-                                                    </span>
-                                                </button>
-                                                </div>
-                                                {/*body*/}
-                                               
-                                                <div class="relative p-6 flex-auto">
-                                                    <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Room name</label>
-                                                    <input type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter room name" required
-                                                    onChange={event => setRoomName(event.target.value)}></input>
-                                                </div>
-                                               
-
-                                                <div class="relative p-6 flex-auto">
-                                                    <p class="my-4 text-slate-500 text-lg leading-relaxed">
-                                                    I always felt like I could do anything. That’s the main
-                                                    thing people are controlled by! Thoughts- their perception
-                                                    of themselves! They're slowed down by their perception of
-                                                    themselves. If you're taught you can’t do anything, you
-                                                    won’t do anything. I was taught I could do everything.
-                                                    </p>
-                                                </div>
-                        
-                                                
-                                                {/*footer*/}
-                                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                                                <button
-                                                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                                    type="button"
-                                                    onClick={() => setShowModal(false)}
-                                                >
-                                                    Close
-                                                </button>
-                                                <button
-                                                    className="bg-yellow-400 text-white active:bg-yellow-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                                    type="button"
-                                                    onClick={() => {handleCreateRoom(roomName); setShowModal(false)}}
-                                                >
-                                                    Save Changes
-                                                </button>
-                                                </div>
-                                            </div>
-                                            </div>
-                                        </div>
-                                        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-                                       
-                                    </>
-                                ) : null}
-                           
-                            </>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            ) : null}
+        
+        </>
     );
 };
 
@@ -223,10 +243,10 @@ const RoomSelect = () => {
                     />
                 ) : (
                     <RoomPage 
-                    roomList={roomList}
-                    avarta={'logo_profile_static_avatar.svg'}
-                    displayName={displayName}
-                />
+                        roomList={roomList}
+                        avarta={'logo_profile_static_avatar.svg'}
+                        displayName={displayName}
+                    />
                 )}
                    
                 </div>
