3a6cfddeef5340918a3bb3617a4d1998916d0fdf
Phuoc T. Nguyen <55012667+jesuispius@users.noreply.github.com>
2022-05-09 15:09:49 -0700

    update style

--- src/pages/Requests.js
+++ src/pages/Requests.js
@@ -364,101 +364,36 @@ const RequestGroupList = () => {
 
     return (
         <>
-            <div className="lg:flex lg:items-center lg:justify-between px-4">
+            <div className="md:flex md:items-center md:justify-between px-4">
                 <div className="flex-1 min-w-0"></div>
-                <div className="mt-3 flex lg:mt-0 lg:ml-2">
-                    <span className="hidden sm:block">
+                <div className="mt-3 grid md:flex md:mt-0 md:ml-2 justify-center text-center">
+                    <span className="sm:block">
                         <button
                             type="button"
                             onClick={handleSnapshot}
-                            className="inline-flex items-center px-3 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            className="w-full justify-center inline-flex items-center px-3 py-2 border border-orange-300 rounded-md shadow-sm text-sm font-medium text-white bg-gradient-to-r from-orange-400 to-rose-400 hover:bg-orange-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
                         >
                             <CameraIcon
-                                className="-ml-1 mr-2 h-5 w-5 text-gray-500"
+                                className="-ml-1 mr-2 h-5 w-5 text-white"
                                 aria-hidden="true"
                             />
                             Snapshot
                         </button>
                     </span>
 
-                    <span className="hidden sm:block ml-2">
+                    <span className="sm:block md:ml-2">
                         <button
                             type="button"
                             onClick={handleRecVideo}
-                            className="inline-flex items-center px-3 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            className="w-full justify-center inline-flex items-center px-3 py-2 border border-orange-300 rounded-md shadow-sm text-sm font-medium text-white bg-gradient-to-r from-orange-400 to-rose-400 hover:bg-orange-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
                         >
                             <VideoCameraIcon
-                                className="-ml-1 mr-2 h-5 w-5 text-gray-500"
+                                className="-ml-1 mr-2 h-5 w-5 text-white"
                                 aria-hidden="true"
                             />
                             Record Video
                         </button>
                     </span>
-
-                    <span className="sm:ml-2">
-                        <button
-                            type="button"
-                            className="inline-flex items-center px-3 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            <CloudIcon
-                                className="-ml-1 mr-2 h-5 w-5"
-                                aria-hidden="true"
-                            />
-                            Recordings
-                        </button>
-                    </span>
-
-                    {/* Dropdown */}
-                    <Menu as="span" className="ml-3 relative sm:hidden">
-                        <Menu.Button className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            More
-                            <ChevronDownIcon
-                                className="-mr-1 ml-2 h-5 w-5 text-gray-500"
-                                aria-hidden="true"
-                            />
-                        </Menu.Button>
-
-                        <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-200"
-                            enterFrom="transform opacity-0 scale-95"
-                            enterTo="transform opacity-100 scale-100"
-                            leave="transition ease-in duration-75"
-                            leaveFrom="transform opacity-100 scale-100"
-                            leaveTo="transform opacity-0 scale-95"
-                        >
-                            <Menu.Items className="origin-top-right absolute right-0 mt-2 -mr-1 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button
-                                            type="button"
-                                            onClick={handleSnapshot}
-                                            className={classNames(
-                                                active ? 'bg-gray-100' : '',
-                                                'block px-4 py-2 text-sm text-gray-700'
-                                            )}
-                                        >
-                                            Snapshot
-                                        </button>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button
-                                            type="button"
-                                            onClick={handleRecVideo}
-                                            className={classNames(
-                                                active ? 'bg-gray-100' : '',
-                                                'block px-4 py-2 text-sm text-gray-700'
-                                            )}
-                                        >
-                                            Recording Video
-                                        </button>
-                                    )}
-                                </Menu.Item>
-                            </Menu.Items>
-                        </Transition>
-                    </Menu>
                 </div>
             </div>
             <br />
