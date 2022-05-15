4824edad08eee91bddc941a64c3f71751f33b17b
TuanDinh140194 <tnd@pdx.edu>
2022-05-15 14:22:15 -0700

    Update profile and login

--- src/components/Login/logIn.js
+++ src/components/Login/logIn.js
@@ -272,7 +272,7 @@ export const Login = () => {
                                     id="button2"
                                     type="submit">
 
-                                <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-yellow-300 group-hover:h-full"></span>
+                                <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-yellow-400 group-hover:h-full"></span>
                                 <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
                                 <svg class="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                                 </span>
