f05659e1aae5135f08f331e3f1dc758119519f98
TuanDinh140194 <tnd@pdx.edu>
2022-05-26 21:52:01 -0700

    make it better

--- src/components/Login/logIn.js
+++ src/components/Login/logIn.js
@@ -230,7 +230,7 @@ export const Login = () => {
                         </FormGroup>
 
                         <div className="text-center">
-                            <button className="h-4 relative inline-flex items-center justify-start py-3 pl-4 pr-12 pt-4 mt-4 overflow-hidden font-semibold text-black transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
+                            {/* <button className="h-4 relative inline-flex items-center justify-start py-3 pl-4 pr-12 pt-4 mt-4 overflow-hidden font-semibold text-black transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
                                     id="button2"
                                     type="submit">
 
@@ -242,7 +242,10 @@ export const Login = () => {
                                 <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                                 </span>
                                 <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">Login</span>
-                            </button>
+                            </button> */}
+                            <Button
+                            id="button2"
+                            type="submit">Signin</Button>
                         </div>
 
                         <FormGroup className="pt-4 text-center my-2">
