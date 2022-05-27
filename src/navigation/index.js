fa507c66766b2d068815e76f925ec1a0a89f05a7
TuanDinh140194 <tnd@pdx.edu>
2022-05-27 16:25:23 -0700

    make it better

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -25,7 +25,7 @@ class Navigation extends Component {
 						<Route exact path="/login" component={Login} />
 						<Route exact path='/homepage' component={Home} />
 						<Route exact path='/requests' component={Requests} />
-						<Route exact path='/profile' component={Profile} />
+						{/* <Route exact path='/profile' component={Profile} /> */}
 						{/* <Route exact path='/setting' component={Setting} /> */}
 						<Route exact path='/room' component={RoomPage} />
 						<Route path="/404" component={Page404} />
